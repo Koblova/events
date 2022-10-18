06d186672b0a6cc712e0fe7aa7b436119d0a6d20
Alina <alina.kirov@mail.ru>
2022-10-18 23:39:39 +0300

    правки

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let square = document.querySelector('#square');
 let circle = document.querySelector('#circle');
 let eBtn = document.querySelector('#e_btn');
 let range = document.querySelector('#range');
+let text = document.querySelector('#range-span')
 
 btn.onclick = function() {
     square.style.backgroundColor = input.value;
@@ -13,7 +14,8 @@ btn.onclick = function() {
 
 eBtn.style.display = "none";
 
-range.addEventListener ("change", function () {
+range.addEventListener ('mousemove', function () {
     circle.style.height = range.value + '%';
     circle.style.width = range.value + '%';
+    text.innerHTML = range.value + '%';
  });
